a9338b67fa62445c1cbe180bc9e9415be95c629f
Patrick Kenneally <pkenneally93@gmail.com>
2026-09-15 02:14:18 +0100

    fix(elements): Rename field 'child' to 'children' in Div struct for clarity

--- src/elements/div.rs
+++ src/elements/div.rs
@@ -2,13 +2,12 @@ use crate::traits::child::Child;
 use crate::traits::clickable::no_clickable::NoClickable;
 
 pub struct Div<C: Child> {
-    pub child: C,
+    pub children: C,
 }
-
 
 impl<C: Child> Child for Div<C> {
     fn render(&self) -> String {
-        format!("<div>{}</div>", self.child.render())
+        format!("<div>{}</div>", self.children.render())
     }
 }
 
