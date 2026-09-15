0e57c028ad0fd6b0489ab463bfb8281b0d9761bf
Patrick Kenneally <pkenneally93@gmail.com>
2026-09-15 02:14:24 +0100

    feat(main): Refactor main function to use Children struct for managing child elements

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 mod elements;
 mod traits;
 
+use elements::button::Button;
+use elements::children::Children;
 use elements::div::Div;
 use elements::text::Text;
 use traits::child::Child;
 
-use crate::elements::a::A;
-
 fn main() {
-    let page = Div {
-        child: Div {
-            child: A {
-                href: String::from("https://example.com"),
-                child: Text {
-                    child: String::from("Click me"),
-                },
+    let children = Children::new()
+        .push(Text {
+            child: String::from("Hello "),
+        })
+        .push(Button {
+            child: Text {
+                child: String::from("Click me"),
             },
-        },
-    };
+        });
+
+    let page = Div { children };
 
     println!("{}", page.render());
 }
